d882e0f3358320252115e48a7828747080e1979f
Mark <mark.bellhouse@gmail.com>
2014-04-28 21:12:36 +1200

    More SMO shite
    SMO dll's hide all over the place... hope appharbor can make do with sql
    10 dll's!

--- MB.LibraryRss.WebUi/Controllers/HomeController.cs
+++ MB.LibraryRss.WebUi/Controllers/HomeController.cs
@@ -18,16 +18,16 @@
 
     public ActionResult Index()
     {
-      return View(this.persistanceService.GetLatest());
+      return this.View(this.persistanceService.GetLatest());
     }
 
     [System.Web.Http.HttpGet, System.Web.Http.HttpPost]
     public JsonResult RefreshTitles()
     {
       // var titles = titleSearchService.GetTitlesFromFile(@"C:\_Marks\Dev\Test\MB.LibraryRss\MB.LibraryRss.WebUi\rss.xml");
-      var titles = titleSearchService.GetTitlesFromUrl(@"https://ent.kotui.org.nz/client/rss/hitlist/pn/qu=newbks-pn&dt=list&st=PD");      
+      var titles = this.titleSearchService.GetTitlesFromUrl(@"https://ent.kotui.org.nz/client/rss/hitlist/pn/qu=newbks-pn&dt=list&st=PD");
 
-      persistanceService.Save(titles);
+      this.persistanceService.Save(titles);
 
       // get async thing working.      
       return new JsonResult { Data = titles, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
